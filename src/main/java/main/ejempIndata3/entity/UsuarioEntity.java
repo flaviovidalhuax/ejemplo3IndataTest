95150c835b28d9e9de7cb0b38965df281b0825c6
flaviovidalhuax <flaviovidalhuax@gmail.com>
2024-02-17 17:15:10 -0600

    add deploy

--- src/main/java/main/ejempIndata3/entity/UsuarioEntity.java
+++ src/main/java/main/ejempIndata3/entity/UsuarioEntity.java
@@ -1,9 +1,6 @@
 package main.ejempIndata3.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,12 +14,11 @@ import org.springframework.stereotype.Component;
 
 @Builder
 @Component
+@Table(name = "Usuario")
 public class UsuarioEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String lastname;
     private String password;
